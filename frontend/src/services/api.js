f3f496161d8ebea11335f692d418d40759a21190
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2020-04-07 07:03:12 -0300

    build finished

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:
-    process.env.REACT_APP_ENV === 'production'
-      ? process.env.REACT_APP_API_URL
-      : 'http://localhost:3333',
+  baseURL: 'https://be-the-hero-jvictorfarias.herokuapp.com',
 });
 
 export default api;
